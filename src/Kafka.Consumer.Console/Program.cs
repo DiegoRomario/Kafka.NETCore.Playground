fa3b05ad051582e8b181dbf50784fbb51d9b301d
Diego Romario <diego.romario@outlook.com>
2020-04-16 21:06:22 -0300

    Initial

---
+++ src/Kafka.Consumer.Console/Program.cs
@@ -0,0 +1,44 @@
+﻿using Confluent.Kafka;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Kafka.Consumer
+{
+    class Program
+    {
+        static Task Main(string[] args)
+        {
+            Console.WriteLine("Starting the service...");
+            Thread.Sleep(2000);
+            Console.WriteLine("Service is running...");
+            var conf = new ConsumerConfig
+            {
+                GroupId = "test-consumer-group",
+                BootstrapServers = "localhost:9092",
+                AutoOffsetReset = AutoOffsetReset.Earliest
+            };
+
+            using (var c = new ConsumerBuilder<Ignore, string>(conf).Build())
+            {
+                c.Subscribe("my_demo_topic");
+                var cts = new CancellationTokenSource();
+
+                try
+                {
+                    while (true)
+                    {
+                        ConsumeResult<Ignore, string> message = c.Consume(cts.Token);
+                        Console.WriteLine($"Message: {message.Message.Value} received from {message.TopicPartitionOffset}");
+                    }
+                }
+                catch (OperationCanceledException)
+                {
+                    c.Close();
+                }
+            }
+
+            return Task.CompletedTask;
+        }
+    }
+}
---
+++ src/Kafka.Producer.API/Controllers/ProducerController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Confluent.Kafka;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Kafka.Producer.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProducerController : ControllerBase
+    {
+        [HttpPost]
+        [ProducesResponseType(typeof(string), 201)]
+        [ProducesResponseType(400)]
+        [ProducesResponseType(500)]
+        public IActionResult Post([FromBody] string message)
+        {
+            return Created("", SendMessageByKafka(message));
+        }
+
+        private string SendMessageByKafka(string message)
+        {
+            var config = new ProducerConfig { BootstrapServers = "localhost:9092" };
+
+            using (var producer = new ProducerBuilder<Null, string>(config).Build())
+            {
+                try
+                {
+                    var sendResult = producer
+                                        .ProduceAsync("my_demo_topic", new Message<Null, string> { Value = message })
+                                            .GetAwaiter()
+                                                .GetResult();
+
+                    return $"Message '{sendResult.Value}' by '{sendResult.TopicPartitionOffset}'";
+                }
+                catch (ProduceException<Null, string> e)
+                {
+                    Console.WriteLine($"The message failed: {e.Error.Reason}");
+                }
+            }
+
+            return string.Empty;
+        }
+
+    }
+}
